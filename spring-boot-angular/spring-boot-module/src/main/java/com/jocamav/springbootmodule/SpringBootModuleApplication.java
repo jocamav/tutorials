6a49bb1e21d2dae04c37b99a41ddfdc5a58f3398
Carlos Martinez <jose.carlos.martinez@gmail.com>
2020-01-11 21:00:49 +0100

    Adding Spring Boot + Angular

---
+++ spring-boot-angular/spring-boot-module/src/main/java/com/jocamav/springbootmodule/SpringBootModuleApplication.java
@@ -0,0 +1,13 @@
+package com.jocamav.springbootmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootModuleApplication.class, args);
+	}
+
+}
---
+++ spring-boot-angular/spring-boot-module/src/test/java/com/jocamav/springbootmodule/SpringBootModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jocamav.springbootmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

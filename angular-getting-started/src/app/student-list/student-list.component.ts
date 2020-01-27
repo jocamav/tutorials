bdbc918ba34ec43b12b4324d8522c0592cb96fa7
MARTINEZ VEGA Jose Carlos <Jose-Carlos.MARTINEZ-VEGA@ext.ec.europa.eu>
2020-01-27 15:38:41 +0100

    Adding a new component

---
+++ angular-getting-started/src/app/student-list/student-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-student-list',
+  templateUrl: './student-list.component.html',
+  styleUrls: ['./student-list.component.css']
+})
+export class StudentListComponent implements OnInit {
+
+  students = [
+    'Harry Potter',
+    'Ron Weasley',
+    'Hermione Granger',
+    'Draco Malfoy',
+    'Cedric Diggory'
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
